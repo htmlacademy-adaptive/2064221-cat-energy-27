87e0022957ed3fefde14451556d974657a7c26f2
ElviraFedchenko <113967658+ElviraFedchenko@users.noreply.github.com>
2023-01-17 22:26:37 +0300

    добавляет автоматизацию сборки

--- gulpfile.js
+++ gulpfile.js
@@ -88,6 +88,7 @@ const copy = (done) => {
 gulp.src([
 'source/fonts/*.{woff2,woff}',
 'source/*.ico',
+'source/*.webmanifest',
 ], {
 base: 'source'
 })
@@ -135,14 +136,14 @@ gulp.watch('source/*.html', gulp.series(html, reload));
 export const build = gulp.series(
 clean,
 copy,
-optimizeImages,
+// optimizeImages,
 gulp.parallel(
 styles,
 html,
 scripts,
 svg,
 sprite,
-createWebp
+// createWebp
 ),
 );
 
