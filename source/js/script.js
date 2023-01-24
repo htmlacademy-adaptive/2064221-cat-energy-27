e2f1d5af31b6df42269bd53be6332fcc84f5e8da
ElviraFedchenko <113967658+ElviraFedchenko@users.noreply.github.com>
2023-01-24 14:09:10 +0300

    вносит последние правки перед проверкой

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,6 @@
+document.querySelector('body').classList.remove('nojs')
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
-
-navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
